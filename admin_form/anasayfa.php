81067a32e9d5ebdd891b8ea42fd7f0fc91a685fd
Ömer Kaymak <omerahmetkaymak@hotmail.com>
2023-08-08 01:56:22 +0300

    Ana sayfa tasarımın yapıldı.

--- admin_form/anasayfa.php
+++ admin_form/anasayfa.php
@@ -20,7 +20,7 @@ if(empty($_SESSION['kul_eposta'])){
     <!--Sidebar Bitiş-->  
 
     <!-- Ana İçerik -->
-    <div class="col-lg-9 mx-md-5 mt-5 col-md-7 col-sm-10 mx-sm-auto">
+    <div class="col-lg-9 mx-md-5 mt-4 col-md-7 col-sm-10 mx-sm-auto">
         <div class="row mx-auto">
             <div class="col-3 container-fluid kart_main mt-xl-1 mt-lg-5 mt-md-5">
                 <h4 class="mt-4">&nbsp;1 Aylık Gelir/Gider</h4><br>
@@ -40,11 +40,20 @@ if(empty($_SESSION['kul_eposta'])){
                 <p style="font-size:1.25rem">&nbsp;&nbsp;<i class="bi bi-currency-euro text-left"></i>&nbsp;&nbsp;&nbsp;&nbsp;1000 TL</p>
             </div>
         </div>
-    </div>
-    <div class="col-lg-9 mx-md-5 mt-5 col-md-7 col-sm-10 mx-sm-auto">
-        <div class="row mx-auto mt-5">
-            <div class="col-6 container-fluid mt-0 mt-lg-5">
-
+        <div class="row mx-auto">
+            <div class="col-lg-4 container-fluid mt-0 mt-lg-5 col-md-10 kart_main_2">
+                <h4>Hava Durumu</h4>
+            </div>
+            <div class="col-lg-8 container-fluid mt-0 mt-lg-5 col-md-10 kart_main_3 mt-lg-0 mt-sm-5">
+                <h4>Son İşlemler</h4>
+            </div>
+        </div>
+        <div class="row mx-auto">
+            <div class="col-lg-8 container-fluid mt-0 mt-lg-5 col-md-10 kart_main_3">
+                <h4>Öğrenci Listesi</h4>
+            </div>
+            <div class="col-lg-4 container-fluid mt-0 mt-lg-5 col-md-10 kart_main_2 mt-lg-0 mt-sm-5">
+                <h4>Aylık Ödeme İşlemleri</h4>
             </div>
         </div>
     </div>
